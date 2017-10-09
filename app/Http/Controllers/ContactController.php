17155ec0e1840342fe8c36b15e6e17364d7e4646
Voeun So <voeun@idcrm.com>
2017-10-09 15:24:48 +0700

    test queue push treatment back

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,6 +18,7 @@ use App\Jobs\ContactAndCard;
 use App\Jobs\Card;
 use App\Http\Service\ClientService;
 use Illuminate\Support\Facades\Mail;
+use App\Jobs\PushLoyaltyCard;
 class ContactController extends Controller
 {
 
@@ -46,26 +47,16 @@ class ContactController extends Controller
     public function test_mail()
     {
 
-        $connection = $this->_getConnection("connection.txt");
-        $condition['idcrm_ruletype'] = 527210007;
-        $client = new ClientService("hariservice.umanota@haricrm.com","Nightfa1","https://haricrm.crm5.dynamics.com");
-        $programRule = $client->retriveCrmData("new_loyaltyprogramrule", $condition);
+        $pusback = array(
+            "idcrm_viptreament" => VALUE_IDCRM_VIP_TREAMENT_YES,
+            "idcrm_lastuseddate" => time() + date("HKT")
+        );
 
-        $loyalty_condition['idcrm_loyaltycardid'] = "dac51908-58a7-e711-8166-e0071b67cb31";
-        $card = $client->retriveCrmData("idcrm_loyaltycard", $loyalty_condition);
+        $this->dispatch(new PushLoyaltyCard(CRM_USER, CRM_PASSWORD, CRM_MODE, CRM_URL, CRM_ORG, '46b1f359-c9ac-e711-8155-e0071b67cb41', "idcrm_loyaltycard", $pusback));
 
-        if(!empty($programRule)) {
-            $voucher = $connection->entity('idcrm_loyaltyvoucher');
-            foreach ($programRule as $key => $loyaltyProgramRule) {
-                if(isset($loyaltyProgramRule['new_loyaltyprogramruleid']) and
-                    !empty($loyaltyProgramRule['new_loyaltyprogramruleid']) and
-                    $loyaltyProgramRule['new_loyaltyprogramruleid']>0){
-                    $totalearnpoint = isset($card[0]['idcrm_totalpoints'])?$card[0]['idcrm_totalpoints']:0;
-                    dd($loyaltyProgramRule['idcrm_pointearned']+ $totalearnpoint);
-                }
 
-            }
-        }
+
+
 
 
     }
