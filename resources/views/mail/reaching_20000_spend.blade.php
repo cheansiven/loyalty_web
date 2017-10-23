d538e0c472abfe3340bc1809349baf47415197e6
Morgane Danis <morgane@viazul.hk>
2017-10-23 14:08:41 +0700

    Add a content line ‘Remember to show...’ on the reaching_20000_spend

--- resources/views/mail/reaching_20000_spend.blade.php
+++ resources/views/mail/reaching_20000_spend.blade.php
@@ -364,16 +364,17 @@
                             <div class="article" style="position: relative;background:#ffffff;line-height: 30px;padding-top: 10px;padding-left: 10px;padding-right: 10px;color: #000;">
 
                                 <p><b>Olá  {{ $contact_name }},</b></p>
-                                <p>Hey Big Spender! To say thank you for spending your hard earned cash with us so regularly we’ve got a few Uma Nota goodies for you to enjoy when you visit us next.</p>
-                                <p>Check your email inbox for your <b>VIP Treatment</b> voucher as you’ve also unlocked privileges to;</p>
+                                <p>Hey Big Spender! To say thank you for spending your hard earned cash with us so regularly we’ve got a few Uma Nota goodies for you to enjoy when you visit us next.</p><br>
+                                <p>Check your email inbox for your <b>VIP Treatment</b> details as you’ve also unlocked privileges to:</p><br>
                                 <p style="padding-left: 20px;">
-                                &nbsp;&nbsp;&nbsp;1. Priority waitlist placement when you message +852 6053 1193 (min.1 hour before arrival)<br/>
+                                &nbsp;&nbsp;&nbsp;1. Priority waitlist placement when you message +852 6053 1193 (min. 1 hour before restaurant hours i.e. <b> 5pm Mon - Sat evenings, 11am Saturday brunch)</b><br/>
                                 &nbsp;&nbsp;&nbsp;2. Early bird access/ priority on event bookings<br/>
                                 &nbsp;&nbsp;&nbsp;3. Exclusive access to future events/ collaborations at Uma Nota<br/>
                                 </p>
-                                <p>Terms and Condition apply see voucher.</p>
+                                <p>Terms and Conditions apply see voucher.</p><br>
                                 <p>To download your voucher, click the button below to add it to your wallet. If you don’t use an iPhone, the card will be displayed as a HTML page or download <b>Wallet Passes | Passbook Wallet</b> for convenience.</p>
                                 <p><a href="{{ $url }}" target="_blank" style="color:#BC9B5D">Click here</a></p>
+                                <p>Remember to show your card to our team on arrival each time you visit. </p><br>
                                 <p>Obrigado! & see you soon,</p><br/>
                                 <p><img src="https://umanota.haricrm.com/image/gcrc/Paulistas_Clube_Email_Sig.png" /></p>
                             </div>
